5c88ec5f024fea06e5d8f0e14480922ce24cecd2
zane <neuschuler@gmail.com>
2018-09-17 22:19:23 -0700

    @ianmitchell helped me get the async working

--- server.js
+++ server.js
@@ -73,56 +73,60 @@ app.get("/scrape", function(req, res) {
 });
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-  // Grab every document in the Articles collection
-  axios.get("https://www.gamespot.com/news/").then(function (response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
+app.get("/articles", function (req, res) {
+      // Grab every document in the Articles collection
+      axios.get("https://www.gamespot.com/news/").then(function (response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $(".media-article").each(function (i, element) {
-      // Save an empty result object
-      var result = {};
+        const queries = []; // <-- THIS
+
+        // Now, we grab every h2 within an article tag, and do the following:
+        $(".media-article").each(function (i, element) {
+          // Save an empty result object
+          var result = {};
 
 
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .attr("data-event-title");
-      result.link = "https://www.gamespot.com" + $(this)
-        .children("a")
-        .attr("href");
-      result.summary = $(this).children("a").children(".media-body").children(".media-deck").text().trim();
+          // Add the text and href of every link, and save them as properties of the result object
+          result.title = $(this)
+            .children("a")
+            .attr("data-event-title");
+          result.link = "https://www.gamespot.com" + $(this)
+            .children("a")
+            .attr("href");
+          result.summary = $(this).children("a").children(".media-body").children(".media-deck").text().trim();
 
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function (dbArticle) {
-          // View the added result in the console
-          console.log("Article saved");
+          // Create a new Article using the `result` object built from scraping
+          queries.push(db.Article.create(result) // <--- THIS
+            .then(function (dbArticle) {
+              // View the added result in the console
+              console.log("Article saved");
+            })
+            .catch(function (err) {
+              // If an error occurred, send it to the client
+              return res.json(err);
+            }));
+        });
+
+        Promise.all(queries).then(function () { // <-- AND THIS
+          console.log("done");
+          db.Article.find({}).sort({
+              added: 1
+            })
+            .then(function (dbArticle) {
+              // If we were able to successfully find Articles, send them back to the client
+              res.json(dbArticle);
+            })
+            .catch(function (err) {
+              // If an error occurred, send it to the client
+              res.json(err);
+            })
         })
-        .catch(function (err) {
-          // If an error occurred, send it to the client
-          return res.json(err);
-        })
-    });
-      console.log("done");
-      db.Article.find({}).sort({
-          added: 1
-        })
-        .then(function (dbArticle) {
-          // If we were able to successfully find Articles, send them back to the client
-          res.json(dbArticle);
-        })
-        .catch(function (err) {
-          // If an error occurred, send it to the client
-          res.json(err);
-        })
 
 
-    // If we were able to successfully scrape and save an Article, send a message to the client\
-  });
-  
+        // If we were able to successfully scrape and save an Article, send a message to the client\
+      });
 
 });
 
